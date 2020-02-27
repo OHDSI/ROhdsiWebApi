94980a6294efde5999f52c6e45cbc90888d44dce
Gowtham Rao <grao9@its.jnj.com>
2020-02-27 06:23:25 -0500

    Revert "made explicit reference to tempdir"
    This reverts commit 184fdba2535cc38c483b486b8548de5ac11c73e4.

--- R/ConceptSet.R
+++ R/ConceptSet.R
@@ -355,8 +355,7 @@ resolveConceptSetId <-
     tempFile <-
       tempfile(
         pattern = paste0("conceptSet_", setId),
-        fileext = ".zip",
-        tempdir = tempdir
+        fileext = ".zip"
       )
     base::writeBin(object = bin, con = file.path(tempFile))
     files <-
