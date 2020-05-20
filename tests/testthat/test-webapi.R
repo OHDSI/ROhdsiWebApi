74982c246a7440ef537ef44d0448cb323594a70f
Schuemie <MSCHUEMI@its.jnj.com>
2020-05-20 14:15:42 +0200

    Adding some more unit tests

--- tests/testthat/test-webapi.R
+++ tests/testthat/test-webapi.R
@@ -22,3 +22,70 @@ test_that("Test getPriorityVocabularyKey", {
   expect_gt(nchar(key), 0)
 })
 
+test_that("Test getConceptSetDefinition", {
+  skip_if(baseUrl == "")
+  conceptSet <- getConceptSetDefinition(1863526, baseUrl = baseUrl)
+  expect_type(conceptSet, "list")
+  expect_equal(conceptSet$id, 1863526)
+  
+  table <- convertConceptSetDefinitionToTable(conceptSet)
+  expect_s3_class(table, "data.frame")
+  expect_gt(nrow(table), 0)
+})
+
+test_that("Test resolveConceptSet", {
+  skip_if(baseUrl == "")
+  conceptSet <- getConceptSetDefinition(1863526, baseUrl = baseUrl)
+  conceptIds <- resolveConceptSet(conceptSet, baseUrl = baseUrl)
+  expect_type(conceptIds, "integer")
+  expect_gt(length(conceptIds), 0)
+})
+
+test_that("Test getConcepts", {
+  skip_if(baseUrl == "")
+  concepts <- getConcepts(conceptIds = c(8507, 8532), baseUrl = baseUrl)
+  expect_true("MALE" %in% concepts$conceptName)
+  expect_true("FEMALE" %in% concepts$conceptName)
+})
+
+test_that("Test getSourceConcepts", {
+  skip_if(baseUrl == "")
+  concepts <- getSourceConcepts(conceptIds = c(8507, 8532), baseUrl = baseUrl)
+  expect_true("MALE" %in% concepts$conceptName)
+  expect_true("FEMALE" %in% concepts$conceptName)
+})
+
+test_that("Test createConceptSetWorkbook", {
+  skip_if(baseUrl == "")
+  file <- tempfile(fileext = ".xlsx")
+  createConceptSetWorkbook(conceptSetIds = c(1863526),
+                           fileName = file,
+                           baseUrl = baseUrl,
+                           included = TRUE,
+                           mapped = TRUE)
+  expect_true(file.exists(file))
+  unlink(file)
+})
+
+test_that("Test getCohortDefinition", {
+  skip_if(baseUrl == "")
+  cohort <- getCohortDefinition(1774139, baseUrl = baseUrl)
+  expect_type(cohort, "list")
+  expect_equal(cohort$id, 1774139)
+})
+
+test_that("Test getCohortGenerationInformation", {
+  skip_if(baseUrl == "")
+  info <- getCohortGenerationInformation(1774139, baseUrl = baseUrl)
+  expect_s3_class(info, "data.frame")
+  expect_gt(nrow(info), 0)
+})
+
+# test_that("Test getCohortResults", {
+#   skip_if(baseUrl == "")
+#   info <- getCohortResults(1774139, baseUrl = baseUrl, sourceKey = "SYNPUF5PCT")
+#   expect_s3_class(info, "data.frame")
+#   expect_gt(nrow(info), 0)
+# })
+
+# TODO: add cohort characterization and incidence rates
