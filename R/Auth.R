8f8d9bb559dd5b8270ae5f6bc50c8523b7c4b739
Adam Black <adam.black@odysseusinc.com>
2020-06-18 21:51:26 -0400

    Add argument checks to Auth.R functions. Remove internal use of keyring.

--- R/Auth.R
+++ R/Auth.R
@@ -8,36 +8,43 @@
 #' @template baseUrl 
 #' @param authMethod The method used for authentication to WebAPI. Options are
 #' \itemize{
+#'  \item{"none"}{No authentication. WebAPI security must be disabled.}
 #'  \item{"db"}{Database authentication using Atlas/WebAPI built in auth}
 #'  \item{"AD"}{Active Directory}
 #'  \item{"windows"}{Windows}
-#'  \item{"none"}{No authentication. WebAPI security must be disabled.}
+#'  \item{oauth}{OAuth}
+#'  \item{IAP}{Google Cloud Platform IAP}
+#'  
 #' }
 #' By default the correct authentication method will be guessed.
-#' @param keyringServiceName (Optional) Keyring stores passwords securely. 
-#'                           For some authentication methods it may be convenient 
-#'                           to pass keyring service name instead of entering a password.
+#' 
+#' @template WebApiUsernamePassword
 #'
 #' @return A WebApi connection object
+#' @import getPass getPass
 #' @export
 connectWebApi <- function(baseUrl, 
                           authMethod = guessAuthMethod(),
-                          atlasUsername = NULL,
-                          keyringServiceName = NULL) {
+                          webApiUsername = NULL,
+                          webApiPassword = getPass("WebApi Password")) {
+  
+  checkmate::assertCharacter(baseUrl, len = 1, min.chars = 1)
+  if (substring(baseUrl, nchar(baseUrl)) == "/") stop("baseUrl should not end with a / (slash)")
+  checkmate::assertChoice(authMethod, choices = c("db", "AD", "windows", "none"))
+  checkmate::assert(checkmate::checkCharacter(webApiUsername), checkmate::checkNull(webApiUsername))
+  checkmate::assert(checkmate::checkCharacter(webApiPassword), checkmate::checkNull(webPassword))
+  
   .checkBaseUrl(baseUrl)
   
   # run appropriate auth
-  if (authMethod == "db") {
-    authHeader <- .authDb(baseUrl, atlasUsername, keyringServiceName)
-  } else if(authMethod == "none") {
-    authHeader <- ""
-  } else if (authMethod == "AD") {
-    authHeader <- .authAD(baseUrl, atlasUsername, keyringServiceName)
-  } else {
-    stop(paste("authMethod must be one of: db, AD, windows, none.
-          You passed in", authMethod))
-  }
- 
+  authHeader <- switch(authMethod,
+                       "none" = "",
+                       "db" = .authDb(baseUrl, webApiUsername, webApiPassword),
+                       "AD" = .authAD(baseUrl, webApiUsername, webApiPassword),
+                       "windows" = .authWindows(baseUrl, webApiUsername, webApiPassword),
+                       "oauth" = .authOauth(baseUrl, webApiUsername, webApiPassword),
+                       "IAP" = .authOauth(baseUrl, webApiUsername, webApiPassword))
+  
   structure(list(baseUrl = baseUrl, 
                  authMethod = authMethod,
                  authHeader = authHeader),
@@ -50,17 +57,14 @@ guessAuthMethod <- function() {
 
 #' Authenticate using Atlas database authentication
 #'
-#' @param keyringUsername A Keyring service name to be used to pull a password from keyring
+#' @template BaseUrl
+#' @template WebApiUsername
+#' @template WebApiPassword
 #'
 #' @return a bearer token to be used in http headers in calls to WebAPI
-.authDb <- function(baseUrl, atlasUsername, keyringServiceName) {
-  if (!is.character(atlasUsername)) stop("db authentication requires a username. Please supply one.")
-  
-  if (!is.null(keyringServiceName)) {
-    password <- keyring::key_get(keyringServiceName)
-  } else {
-    password <- getPass::getPass(paste("Please Enter Atlas Password for", atlasUsername))
-  }
+.authDb <- function(baseUrl, webApiUsername, webApiPassword) {
+  checkmate::assertCharacter(webApiUsername, min.chars = 1, len = 1)
+  checkmate::assertCharacter(webApiPassword, min.chars = 1, len = 1)
   
   authUrl <- paste0(baseUrl, "/user/login/db")
   login <- list(login = atlasUsername, password = password)
@@ -68,8 +72,4 @@ guessAuthMethod <- function() {
   if (length(httr::headers(r)$bearer) < 1) stop("db authentication failed.")
   bearer <- paste0("Bearer ", httr::headers(r)$bearer)
   bearer
-}
-
-.authAD <- function(baseUrl, atlasUsername, keyringServiceName) {
-  stop("AD authentication has not been implemented yet")
 }
