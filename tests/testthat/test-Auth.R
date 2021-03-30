6a6b3800b18cf9d2e2726594992f74abf9715ea9
jgilber2 <JGIlber2@its.jnj.com>
2021-03-29 18:17:28 -0700

    Added sleep and random ports to allow plumbr processes to start

--- tests/testthat/test-Auth.R
+++ tests/testthat/test-Auth.R
@@ -2,25 +2,23 @@ library(httr)
 library(testthat)
 
 # start the api simulation in a background R process
-port <- 8000
+port <- httpuv::randomPort(8000, 8080)
 plumberPath <- system.file("WebApiSimulation", "plumber.R", package = "ROhdsiWebApi", mustWork = T)
 
 serverStart <- function(file, port) {
-  pr <- plumber::plumb(file = file)
-  pr$run(port = port, swagger = FALSE)
-  return(pr)
+  plumber::plumb(file = file)$run(port = port, swagger = FALSE)
 }
-webApiSimulation <- callr::r_bg(serverStart, args = list(file = plumberPath, port = port))
-
+webApiSimulation <- callr::r_bg(serverStart, args = list(file = plumberPath, port = port), stderr = "plumbr_error.txt", stdout = "plumbr_out.log")
+Sys.sleep(1)
 simulationBaseUrl <- paste0("http://localhost:", port)
 
 test_that("Test getWebApiVersion", {
-  skip_if_not(webApiSimulation$is_alive())
+  testthat::skip_if(!webApiSimulation$is_alive())
   expect_equal(getWebApiVersion(simulationBaseUrl), "2.7.6")
 })
 
 test_that("Test authorizeWebApi", {
-  skip_if_not(webApiSimulation$is_alive())
+  testthat::skip_if(!webApiSimulation$is_alive())
   
   fakeUrl <- "http://blah.blah"
   setAuthHeader(fakeUrl, "Bearer 0000")
@@ -35,7 +33,7 @@ test_that("Test authorizeWebApi", {
 })
 
 test_that("Test getCdmSources", {
-  skip_if_not(webApiSimulation$is_alive())
+  testthat::skip_if(!webApiSimulation$is_alive())
   expectedCdmSources <- tibble::tribble(
     ~sourceId, ~sourceName, ~sourceKey, ~sourceDialect, ~cdmDatabaseSchema, ~vocabDatabaseSchema, ~resultsDatabaseSchema,
      1,        "synthea",   "synthea",  "postgresql",   "synthea",          "synthea",            "synthea_results",      
@@ -46,15 +44,17 @@ test_that("Test getCdmSources", {
 
 
 test_that("Test getPriorityVocabularyKey", {
-  skip_if_not(webApiSimulation$is_alive())
+  testthat::skip_if(!webApiSimulation$is_alive())
   expect_equal(getPriorityVocabularyKey(simulationBaseUrl), "synthea")
 })
 
 
 test_that("http error codes are handled", {
-  skip_if_not(webApiSimulation$is_alive())
+  testthat::skip_if(!webApiSimulation$is_alive())
   expect_error(stop_for_status(.GET(simulationBaseUrl, path = "echoStatus", query = list(status = 401))), "401")
   expect_error(stop_for_status(.GET(simulationBaseUrl, path = "echoStatus", query = list(status = 403))), "403")
   expect_error(stop_for_status(.GET(simulationBaseUrl, path = "echoStatus", query = list(status = 404))), "404")
   expect_error(stop_for_status(.GET(simulationBaseUrl, path = "echoStatus", query = list(status = 500))), "500")
 })
+
+webApiSimulation$kill()
