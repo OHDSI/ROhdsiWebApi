75bc8ba39005b7bddc0fa50feda969f5a153dcd0
Gowtham Rao <rao@ohdsi.org>
2021-07-14 11:02:44 -0400

    Changing environmental variables

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,20 +1,20 @@
 authType <- getOption("ohdsiWebApiAuthType")
 if (authType == "windows") {
-  testBaseUrl <- Sys.getenv("TEST_OHDSI_WINDOWS_WEB_API_URL")
-  testOhdsiUser <- Sys.getenv("TEST_OHDSI_WINDOWS_WEB_API_USER")
-  testOhdsiPassword <- Sys.getenv("TEST_OHDSI_WINDOWS_WEB_API_PASSWORD")
+  testBaseUrl <- Sys.getenv("WEBAPI_TEST_SECURE_WEBAPI_URL")
+  testOhdsiUser <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_NAME")
+  testOhdsiPassword <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_PASSWORD")
 }
 
 if (authType == "ad") {
-  testBaseUrl <- Sys.getenv("TEST_OHDSI_AD_WEB_API_URL")
-  testOhdsiUser <- Sys.getenv("TEST_OHDSI_AD_WEB_API_USER")
-  testOhdsiPassword <- Sys.getenv("TEST_OHDSI_AD_WEB_API_PASSWORD")
+  testBaseUrl <- Sys.getenv("WEBAPI_TEST_SECURE_WEBAPI_URL")
+  testOhdsiUser <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_NAME")
+  testOhdsiPassword <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_PASSWORD")
 }
 
 if (authType == "db") {
-  testBaseUrl <- Sys.getenv("TEST_OHDSI_DB_WEB_API_URL")
-  testOhdsiUser <- Sys.getenv("TEST_OHDSI_DB_WEB_API_USER")
-  testOhdsiPassword <- Sys.getenv("TEST_OHDSI_DB_WEB_API_PASSWORD")
+  testBaseUrl <- Sys.getenv("WEBAPI_TEST_SECURE_WEBAPI_URL")
+  testOhdsiUser <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_NAME")
+  testOhdsiPassword <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_PASSWORD")
 }
 
 authSet <- !(testBaseUrl == "" | testOhdsiUser == "" | testOhdsiPassword == "")
