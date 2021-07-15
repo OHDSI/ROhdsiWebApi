265eaed88258fb13940db92f9f30f890ac301660
Adam Black <adam.black@odysseusinc.com>
2021-07-15 15:50:17 -0400

    consolidate generation functions into one file.

--- R/Generation.R
+++ R/Generation.R
@@ -1,4 +1,4 @@
-# @file GetGenerationInformation
+# @file InvokeGeneration
 #
 # Copyright 2021 Observational Health Data Sciences and Informatics
 #
@@ -15,6 +15,91 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+#' Invoke generation. \lifecycle{stable}
+#' @details
+#' Invoke generation (execution) information.
+#'
+#' @template BaseUrl
+#' @template Id
+#' @template Category
+#' @template SourceKey
+#' @return
+#' A dataframe with generation information such as status, jobName, and time.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' invokeGeneration(id = 13242, category = "cohort", baseUrl = "http://server.org:80/WebAPI")
+#' }
+#' @export
+invokeGeneration <- function(id, baseUrl, sourceKey, category) {
+  baseUrl <- gsub("/$", "", baseUrl)
+  .checkBaseUrl(baseUrl)
+  
+  arguments <- .getStandardCategories() %>% dplyr::filter(.data$categoryStandard %in% c("cohort",
+                                                                                        "characterization",
+                                                                                        "pathway",
+                                                                                        "incidenceRate"))
+  
+  argument <- arguments %>% dplyr::filter(.data$categoryStandard == category)
+  
+  errorMessage <- checkmate::makeAssertCollection()
+  checkmate::assertInt(id, add = errorMessage)
+  checkmate::assertCharacter(category, add = errorMessage)
+  checkmate::assertSubset(x = category, choices = argument$categoryStandard)
+  checkmate::reportAssertions(errorMessage)
+  
+  if (!all(isValidSourceKey(sourceKeys = sourceKey, baseUrl = baseUrl))) {
+    stop(paste0(sourceKey, " is not present in WebApi."))
+  }
+  
+  urlRoot <- paste0(baseUrl,
+                    "/",
+                    argument$categoryUrl,
+                    "/",
+                    id,
+                    "/",
+                    argument$categoryUrlGeneration)
+  url <- paste0(urlRoot, "/", sourceKey)
+  
+  if (argument$categoryStandard %in% c("cohort", "incidenceRate")) {
+    response <- .GET(url)
+  }
+  if (argument$categoryStandard %in% c("characterization", "pathway")) {
+    response <- .POST(url)
+  }
+  if (!response$status_code == 200) {
+    if (isValidId(ids = id, baseUrl = baseUrl, category = category)) {
+      error <- paste0(argument$categoryFirstUpper, " ", id, " is not present in WebApi.")
+    } else {
+      error <- ""
+    }
+    stop(paste0(error, response$status_code))
+  }
+  response <- httr::content(response)
+  response <- response %>% purrr::map(function(x) {
+    purrr::map(x, function(y) {
+      ifelse(is.null(y), NA, y)
+    })
+  }) %>% unlist(recursive = TRUE,
+                use.names = TRUE) %>% as.matrix() %>% t() %>% tidyr::as_tibble() %>%
+    .removeStringFromDataFrameName(string = "jobInstance.") %>% .removeStringFromDataFrameName(string = "jobParameters.") %>%
+    dplyr::rename_at(dplyr::vars(dplyr::contains("_")),
+                     .funs = SqlRender::snakeCaseToCamelCase) %>%
+    utils::type.convert(as.is = TRUE, dec = ".") %>% .addSourceKeyToSourceId(baseUrl = baseUrl) %>%
+    .addSourceNameToSourceKey(baseUrl = baseUrl) %>% .normalizeDateAndTimeTypes()
+  
+  writeLines(paste0("Generation of ",
+                    argument$categoryFirstUpper,
+                    " definition id: ",
+                    id,
+                    " for sourceKey: ",
+                    sourceKey,
+                    " invoked."))
+  return(response)
+}
 
 
 #' Get generation information. \lifecycle{stable}
@@ -39,20 +124,20 @@
 getGenerationInformation <- function(id, category, baseUrl) {
   baseUrl <- gsub("/$", "", baseUrl)
   .checkBaseUrl(baseUrl)
-
+  
   arguments <- .getStandardCategories() %>% dplyr::filter(.data$categoryStandard %in% c("cohort",
                                                                                         "characterization",
                                                                                         "pathway",
                                                                                         "incidenceRate"))
-
+  
   argument <- arguments %>% dplyr::filter(.data$categoryStandard == category)
-
+  
   errorMessage <- checkmate::makeAssertCollection()
   checkmate::assertInt(id, add = errorMessage)
   checkmate::assertCharacter(category, add = errorMessage)
   checkmate::assertSubset(x = category, choices = argument$categoryStandard)
   checkmate::reportAssertions(errorMessage)
-
+  
   urlRoot <- paste0(baseUrl,
                     "/",
                     argument$categoryUrl,
@@ -60,10 +145,10 @@ getGenerationInformation <- function(id, category, baseUrl) {
                     id,
                     "/",
                     argument$categoryUrlGenerationInformation)
-
+  
   cdmSources <- getCdmSources(baseUrl)
   validSourceKeys <- cdmSources %>% dplyr::select(.data$sourceKey) %>% dplyr::distinct() %>% dplyr::pull(.data$sourceKey)
-
+  
   ##### cohort/characterization/pathway ####
   if (argument$categoryStandard %in% c("cohort", "characterization", "pathway")) {
     url <- urlRoot
@@ -83,7 +168,7 @@ getGenerationInformation <- function(id, category, baseUrl) {
       for (i in (1:length(response))) {
         responseAll[[i]] <- response[[i]] %>% purrr::map(function(x) {
           purrr::map(x, function(y) {
-          ifelse(is.null(y), NA, y)
+            ifelse(is.null(y), NA, y)
           })
         }) %>% unlist(recursive = TRUE,
                       use.names = TRUE) %>% as.matrix() %>% t() %>% tidyr::as_tibble() %>%
@@ -99,7 +184,7 @@ getGenerationInformation <- function(id, category, baseUrl) {
       response <- tidyr::tibble()
     }
   }
-
+  
   ##### incidence rate ####
   if (argument$categoryStandard == "incidenceRate") {
     executionInfo <- list()
@@ -121,7 +206,7 @@ getGenerationInformation <- function(id, category, baseUrl) {
       if (length(response$executionInfo) > 0) {
         executionInfo[[i]] <- response$executionInfo %>% purrr::map(function(x) {
           purrr::map(x, function(y) {
-          ifelse(is.null(y), NA, y)
+            ifelse(is.null(y), NA, y)
           })
         }) %>% unlist(recursive = TRUE,
                       use.names = TRUE) %>% as.matrix() %>% t() %>% tidyr::as_tibble() %>%
@@ -147,3 +232,70 @@ getGenerationInformation <- function(id, category, baseUrl) {
                     " had a status = 'COMPLETED'"))
   return(response)
 }
+
+
+#' Invoke generation. \lifecycle{stable}
+#' @details
+#' Invoke generation (execution) information.
+#'
+#' @template BaseUrl
+#' @template Id
+#' @template Category
+#' @template SourceKey
+#' @return
+#' Error message if invoke failed.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' cancelGeneration(id = 13242, category = "cohort", baseUrl = "http://server.org:80/WebAPI")
+#' }
+#' @export
+cancelGeneration <- function(id, baseUrl, sourceKey, category) {
+  baseUrl <- gsub("/$", "", baseUrl)
+  .checkBaseUrl(baseUrl)
+  
+  arguments <- .getStandardCategories() %>% dplyr::filter(.data$categoryStandard %in% c("cohort",
+                                                                                        "characterization",
+                                                                                        "pathway",
+                                                                                        "incidenceRate"))
+  
+  argument <- arguments %>% dplyr::filter(.data$categoryStandard == category)
+  
+  errorMessage <- checkmate::makeAssertCollection()
+  checkmate::assertInt(id, add = errorMessage)
+  checkmate::assertCharacter(category, add = errorMessage)
+  checkmate::assertSubset(x = category, choices = argument$categoryStandard)
+  checkmate::reportAssertions(errorMessage)
+  
+  if (!all(isValidSourceKey(sourceKeys = sourceKey, baseUrl = baseUrl))) {
+    stop(paste0(sourceKey, " is not present in WebApi."))
+  }
+  
+  urlRoot <- paste0(baseUrl, "/", argument$categoryUrl, "/", id, "/", argument$categoryUrlCancel)
+  url <- paste0(urlRoot, "/", sourceKey)
+  
+  if (argument$categoryStandard %in% c("cohort")) {
+    response <- .GET(url)
+  }
+  if (argument$categoryStandard %in% c("characterization", "pathway", "incidenceRate")) {
+    response <- .DELETE(url)
+  }
+  if (!response$status_code %in% c(200, 204)) {
+    if (isValidId(ids = id, baseUrl = baseUrl, category = category)) {
+      error <- paste0(argument$categoryFirstUpper, " ", id, " is not present in WebApi.")
+    } else {
+      error <- ""
+    }
+    stop(error, response$status_code)
+  }
+  warning(paste0("Generation of ",
+                 argument$categoryFirstUpper,
+                 " definition id: ",
+                 id,
+                 " for sourceKey: ",
+                 sourceKey,
+                 " requested to be stopped."))
+  # nothing to return.
+}
+
