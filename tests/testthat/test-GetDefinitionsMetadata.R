d2f9a743e7f7542a58f0d36d9ef0322386266563
martin <lavalleem@mymail.vcu.edu>
2021-07-16 17:14:36 -0400

    add tests for concept set workbook

--- tests/testthat/test-GetDefinitionsMetadata.R
+++ tests/testthat/test-GetDefinitionsMetadata.R
@@ -1,4 +1,4 @@
-with_mock_dir("mocks/DefinitionsMeta", {
+with_mock_dir("mocks/DefinitionsMetadata", {
   test_that("getDefinitionsMetaData works",{
     df <- getDefinitionsMetadata(baseUrl = baseUrl,
                                  category = "cohort")
@@ -12,4 +12,12 @@ with_mock_dir("mocks/DefinitionsMeta", {
     expect_error(getDefinition(baseUrl = misspelledBaseUrl,
                                category = 'cohort'))
   })
+
+  test_that("getDefinitionsMetadata group2", {
+    df <- getDefinitionsMetadata(baseUrl = baseUrl,
+                                 category = "characterization")
+    expect_s3_class(df, "data.frame")
+    expect_equal(nrow(df), 2)
+  })
+  
 })
