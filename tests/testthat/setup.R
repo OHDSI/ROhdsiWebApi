93440bb6b00a5fbfaeb8e16bd4b9026fe2a48bbb
Adam Black <adam.black@odysseusinc.com>
2021-07-16 00:49:24 -0400

    clean up test setup.R file

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,4 +1,5 @@
 ## Setup file for ROhdsiWebApi tests -----------------
+library(httptest)
 
 ## set up Environmental variables
 baseUrl <- Sys.getenv("WEBAPI_TEST_WEBAPI_URL") #nonsecure test environment
@@ -7,54 +8,12 @@ sourceKeyVariable <- 'SYNPUF5PCT' #source db
 # testOhdsiUser <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_NAME") #db test user
 # testOhdsiPassword <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_PASSWORD") # db test password
 
-#preloaded ids careful this is fragile
+# preload ids for tests (careful this is fragile) --------------------
 idCohort <- 2
 idConceptSet <- 2
 idCharacterization <- 1
 idIncidenceRate <- 2
 idPathway <- 1
-
-# library(withr)
-library(httptest)
-# library(httr)
-
-
-# ## load environmental variables for secure sign on-------
-# authType <- getOption("ohdsiWebApiAuthType")
-# # if (authType == "windows") {
-# #   testBaseUrl <- Sys.getenv("WEBAPI_TEST_SECURE_WEBAPI_URL")
-# #   testOhdsiUser <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_NAME")
-# #   testOhdsiPassword <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_PASSWORD")
-# # }
-# # 
-# # if (authType == "ad") {
-# #   testBaseUrl <- Sys.getenv("WEBAPI_TEST_SECURE_WEBAPI_URL")
-# #   testOhdsiUser <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_NAME")
-# #   testOhdsiPassword <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_PASSWORD")
-# # }
-# 
-# if (authType == "db") {
-#   testBaseUrl <- Sys.getenv("WEBAPI_TEST_SECURE_WEBAPI_URL")
-#   testOhdsiUser <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_NAME")
-#   testOhdsiPassword <- Sys.getenv("WEBAPI_TEST_ADMIN_USER_PASSWORD")
-# }
-# 
-# authSet <- !(testBaseUrl == "" | testOhdsiUser == "" | testOhdsiPassword == "")
-
-## set up Environmental variables
-
-
-# if(!dir.exists("tests/testthat/mocks")) {
-#   # if the mocks directory does not exist then we will need access to the secured Webapi test instance
-#   authorizeWebApi(testBaseUrl, "db", testOhdsiUser, testOhdsiPassword)
-# }
-
-#authSet <- !(testBaseUrl == "" | testOhdsiUser == "" | testOhdsiPassword == "")
-#force trigger
-# authSet <- TRUE
-
-#comment test
-
 
 
 ## Set scratch directory to test writes ----------------------
