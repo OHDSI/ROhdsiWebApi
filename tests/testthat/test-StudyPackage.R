fe5e8b92c4c6d0a75f718ce7e73a1f7ffe1ee236
Adam Black <adam.black@odysseusinc.com>
2021-07-18 08:31:15 -0400

    Fix test-StudyPackage.R

--- tests/testthat/test-StudyPackage.R
+++ tests/testthat/test-StudyPackage.R
@@ -2,9 +2,9 @@
 
 with_mock_dir("mocks/StudyPackage", {
 
-  test_that("Test insertCohortDefinitionInPackage", {
+  test_that("insertCohortDefinitionInPackage works", {
     
-    scratch <- tempdir()
+    scratch <- tempdir(check = TRUE)
     jsonFolder <- file.path(scratch, "inst/cohorts")
     sqlFolder <- file.path(scratch, "inst/sql/sql_server")
     
@@ -16,19 +16,19 @@ with_mock_dir("mocks/StudyPackage", {
       "Created JSON file:"
     )
 
-    jsonFile <- readr::read_file(list.files(jsonFolder, full.names = TRUE)[[1]])
+    jsonFile <- readr::read_file(list.files(jsonFolder, full.names = TRUE)[1])
     expect_true(nchar(jsonFile) > 4) 
     
-    sqlFile <- readr::read_file(list.files(sqlFolder, full.names = TRUE)[[1]])
+    sqlFile <- readr::read_file(list.files(sqlFolder, full.names = TRUE)[1])
     expect_true(nchar(jsonFile) > 4) 
     
     unlink(scratch, recursive = TRUE, force = TRUE)
   })
 
-  test_that("test insertCohortDefinitionSetInPackage", {
-    
+  test_that("insertCohortDefinitionSetInPackage works", {
     # create the temp package structure
-    scratch <- tempdir()
+    scratch <- tempdir(TRUE)
+    # scratch <- here::here("tempPackage")
     dir.create(file.path(scratch, "inst", "settings"), recursive = TRUE)
     jsonFolder <- file.path(scratch, "inst", "cohorts")
     dir.create(jsonFolder, recursive = TRUE)
@@ -36,24 +36,32 @@ with_mock_dir("mocks/StudyPackage", {
     dir.create(sqlFolder, recursive = TRUE)
     
     # insert CohortsToCreate.csv file
-    df <- data.frame(atlasId = idCohort, cohortId = idCohort, name = "blah")
+    df <- data.frame(atlasId = idCohort, cohortId = idCohort, name = c("thing1", "thing2"))
     settingsPath <- file.path(scratch, "inst", "settings", "CohortsToCreate.csv")
     write.csv(df, settingsPath, row.names = FALSE)
+    
+    dir.create(file.path(scratch, "R"))
+    rFileName <- file.path(scratch, "R", "CreateCohorts.R")
 
     expect_output(
-      insertCohortDefinitionSetInPackage(fileName = settingsPath,
-                                         baseUrl = baseUrl,
-                                         jsonFolder = jsonFolder,
-                                         sqlFolder = sqlFolder,
-                                         insertCohortCreationR = FALSE,
-                                         packageName = "fakePackage"),
-      "Inserting cohort")
+        insertCohortDefinitionSetInPackage(fileName = settingsPath,
+                                           baseUrl = baseUrl,
+                                           jsonFolder = jsonFolder,
+                                           sqlFolder = sqlFolder,
+                                           rFileName = rFileName,
+                                           generateStats = TRUE,
+                                           packageName = "fakePackage"),
+    "Inserting cohort")
     
+    # Check that the files were created and are not empty
     jsonFile <- readr::read_file(list.files(jsonFolder, full.names = TRUE)[[1]])
-    expect_true(nchar(jsonFile) > 4) 
+    expect_gt(nchar(jsonFile), 4) 
     
     sqlFile <- readr::read_file(list.files(sqlFolder, full.names = TRUE)[[1]])
-    expect_true(nchar(jsonFile) > 4) 
+    expect_gt(nchar(sqlFile), 4) 
+    
+    rFile <- readr::read_file(rFileName)
+    expect_gt(nchar(rFile), 4)
     
     unlink(scratch, recursive = TRUE, force = TRUE)
   })
