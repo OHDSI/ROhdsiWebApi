e724bc346e51c7fd45d1b4b029e8d62515d69bee
jgilber2 <JGIlber2@its.jnj.com>
2021-10-29 13:06:18 -0700

    removed mock tests for updating definitions

--- tests/testthat/test-GetPostDeleteDefinition.R
+++ tests/testthat/test-GetPostDeleteDefinition.R
@@ -3,7 +3,7 @@
 with_mock_dir("mocks/GetPostDeleteDefinition", {
 
   test_that("Get Post and Delete functions work for cohorts", {
-    
+
     # Get the test cohort definition
     def <- getDefinition(idCohort, baseUrl, "cohort")
     expect_type(def, "list")
@@ -13,18 +13,22 @@ with_mock_dir("mocks/GetPostDeleteDefinition", {
     expect_s3_class(response, "data.frame")
     expect_equal(nrow(response), 1)
     expect_equal(response$name, "temporary test cohort")
-
-
-    updef <- getDefinition(response$id, baseUrl, "cohort")
-    updef$name <- "A new test name"
-    expect_output(updateDefintion(updef, baseUrl, "cohort", displayWarnings = FALSE), "Success")
-
-    def <- getDefinition(response$id, baseUrl, "cohort")
-    expect_equal(def$name, updef$name)
     # Delete the cohort
     expect_output(deleteDefinition(response$id, baseUrl, "cohort"), "Success")
     
   })
-
 })
 
+# Mock tests appear to fail for updates
+test_that("Update definition", {
+  def <- getDefinition(idCohort, baseUrl, "cohort")
+  response <- postDefinition(baseUrl, "Original name", "cohort", def)
+
+  updef <- getDefinition(response$id, baseUrl, "cohort")
+  updef$name <- "A new test name"
+  expect_output(updateDefintion(updef, baseUrl, "cohort", displayWarnings = FALSE), "Success")
+
+  def <- getDefinition(response$id, baseUrl, "cohort")
+  expect_equal(def$name, updef$name)
+  deleteDefinition(response$id, baseUrl, "cohort")
+})
