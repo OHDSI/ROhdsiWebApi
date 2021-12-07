2c8c67aef4ee75d4d1b8bc466b9926cb5e1b2a9e
Gowtham Rao <rao@ohdsi.org>
2021-12-07 10:51:37 -0500

    Export cohort set (#235)
    * Code changes to prefer tidy style syntax
    * Auto format using R Studio

--- R/exportCohortDefinitionSet.R
+++ R/exportCohortDefinitionSet.R
@@ -8,39 +8,48 @@
 #' @details
 #' Constructs a CohortDefinition set containing the following fields: \describe{
 #' \item{atlasId}{The cohort ID in
-#' ATLAS.} \item{cohortId}{a copy of the value in atlasId.} \item{cohortName}{The name of the cohort.} 
+#' ATLAS.} \item{cohortId}{a copy of the value in atlasId.} \item{cohortName}{The name of the cohort.}
 #' \item{sql}{The cohort generation sql.} \item{json}{The cohort definition JSON.}
 #' \item{logicDescription}{The cohort description.}}
 #'
 #' @export
-exportCohortDefinitionSet <- function(baseUrl, cohortIds = c(), generateStats) {
-  
-  if (length(cohortIds) == 0) {
-    stop("Must provide a non-zero length cohortIds vector.")
+exportCohortDefinitionSet <-
+  function(baseUrl, cohortIds, generateStats = FALSE) {
+    if (length(cohortIds) == 0) {
+      stop("Must provide a non-zero length cohortIds vector.")
+    }
+    cohortDefinitionSet <- dplyr::tibble(
+      atlasId = integer(),
+      cohortId = integer(),
+      cohortName = character(),
+      sql = character(),
+      json = character(),
+      logicDescription = character()
+    )
+    
+    for (i in (1:length(cohortIds))) {
+      cohortId <- cohortIds[i]
+      ParallelLogger::logInfo(paste("Fetching cohortId:", cohortId))
+      object <-
+        getCohortDefinition(cohortId = cohortId, baseUrl = baseUrl)
+      json <- .toJSON(object$expression, pretty = TRUE)
+      sql <-
+        getCohortSql(
+          baseUrl = baseUrl,
+          cohortDefinition = object,
+          generateStats = generateStats
+        )
+      cohortDefinitionSet <- dplyr::bind_rows(
+        cohortDefinitionSet,
+        dplyr::tibble(
+          atlasId = cohortId,
+          cohortId = cohortId,
+          cohortName = object$name,
+          sql = sql,
+          json = json,
+          logicDescription = ifelse(is.null(object$description), NA, object$description)
+        )
+      )
+    }
+    return(cohortDefinitionSet)
   }
-  cohortDefinitionSet <- data.frame(atlasId = integer(), 
-                                    cohortId = integer(), 
-                                    cohortName = character(), 
-                                    sql=character(), 
-                                    json=character(), 
-                                    logicDescription=character(), stringsAsFactors = FALSE)
-  
-  for (i in 1:length(cohortIds)) {
-    cohortId = cohortIds[i];
-    writeLines(paste("Fetching cohortId:", cohortId));
-    object <- getCohortDefinition(cohortId = cohortId, baseUrl = baseUrl)
-    json <- .toJSON(object$expression, pretty = TRUE)
-    sql <- getCohortSql(baseUrl = baseUrl, cohortDefinition = object, generateStats = generateStats)
-    cohortDefinitionSet <- rbind(cohortDefinitionSet, 
-                                 data.frame(atlasId = cohortId, 
-                                            cohortId = cohortId, 
-                                            cohortName = object$name, 
-                                            sql = sql, 
-                                            json = json,
-                                            logicDescription = ifelse(is.null(object$description), NA, object$description),
-                                            stringsAsFactors = FALSE)
-                                 );
-  }  
-  
-  return(cohortDefinitionSet);
-}
