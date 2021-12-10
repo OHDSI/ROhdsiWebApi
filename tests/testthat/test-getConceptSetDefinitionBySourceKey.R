abfe73794dd7ed477ac61f89de52fad9bfa54e82
Chris Knoll <cknoll@ohdsi.org>
2021-12-09 21:26:17 -0500

    Added tests with sourcekey param.

--- tests/testthat/test-getConceptSetDefinitionBySourceKey.R
+++ tests/testthat/test-getConceptSetDefinitionBySourceKey.R
@@ -1,7 +1,13 @@
 with_mock_dir("mocks/getConceptSetDefinitionBySourceKey", {
   
   test_that("getConceptSetDefinitionBySourceKey works", {
-    conceptSetDefByKey <- ROhdsiWebApi::getConceptSetDefinitionBySourceKey(baseUrl= baseUrl, conceptSetId= 2)
+    conceptSetDefByKey <- ROhdsiWebApi::getConceptSetDefinitionBySourceKey(baseUrl=baseUrl, conceptSetId=2)
+    expect_equal(length(conceptSetDefByKey$expression$items), 1)
+  })
+
+  test_that("getConceptSetDefinitionBySourceKey works with vocabularySourceKey", {
+    conceptSetDefByKey <- ROhdsiWebApi::getConceptSetDefinitionBySourceKey(baseUrl= baseUrl, conceptSetId=2, 
+                                                                           vocabularySourceKey = "SYNPUF5PCT")
     expect_equal(length(conceptSetDefByKey$expression$items), 1)
   })
 })
