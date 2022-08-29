d5c91494a084fef39f93383ca296440bdeff2713
Gowtham Rao <rao@ohdsi.org>
2022-08-29 16:36:00 -0400

    Add some functions related to permissions

--- R/permissions.R
+++ R/permissions.R
@@ -16,74 +16,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' Assign cohort definition permissions to user.
-#'
-#' @details
-#' The function will call the webservice to assign permissions to a cohort definition for the given userId.
-#'
-#' @template baseUrl
-#' @param cohortId    The ID of the cohort definition
-#' @param userId      The userId to grant permission
-#' @param permission  The permission type to assign.  Can be "READ" or "WRITE". Defaults to "WRITE".    
-#'
-#' @examples
-#' \dontrun{
-#' setCohortPermission(baseUrl, cohortId, userId, permission = "WRITE") 
-#' }
-#' @export
-setCohortPermission <- function(baseUrl, cohortId, userId, permission = "WRITE") {
-  
-  .checkBaseUrl(baseUrl)
-  if(!securityEnabled(baseUrl)) {
-    message("Security is not enabled so permissions cannot be set.")
-    return(invisible(NULL))
-  } 
-  
-  if(getWebApiVersion(baseUrl) < "2.10") {
-    message("setCohortPermission can only be used with WebAPI version 2.10 and later")
-    return(invisible(NULL))
-  }
-  
-  
-  if (!permission %in% c("READ", "WRITE")) {
-    stop(paste0("Invalid Permission Type:", permission, ".  Valid types are READ or WRITE."))
-  }
-  
-  url <- paste0(baseUrl, "/permission/access/COHORT_DEFINITION/", cohortId, "/role/",userId)
-  response <- .postJson(url, paste0('{"accessType":"', permission, '"}'))
-  
-  if (response$status_code != 204) {
-    stop(paste0("Error: Failed to assign permissions"))
-  }
-  invisible(response)
-}
-
-#' Get user permission 
+#' Get user information 
 #' 
-#' This function only applies to WebAPI instances with security enabled. If security is not
-#' emabled then it will return an error.
+#' This function returns all public users in the security enabled webapi, and their
+#' public permissions. This function only works in security enabled webapi.
 #'
 #' @template baseUrl 
 #'
-#' @return A dataframe with user ID, user name, and permission information
+#' @return A tibble with user ID, user name, and permission information when available.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' getUserPermissions(baseUrl = "http://server.org:80/WebAPI")
+#' getUserInformations(baseUrl = "http://server.org:80/WebAPI")
 #' }
-getUserPermission <- function(baseUrl) {
+getUserInformation <- function(baseUrl) {
   .checkBaseUrl(baseUrl)
-  if(!securityEnabled(baseUrl)) {
+  if (!securityEnabled(baseUrl)) {
     message("Security is not enabled so there are no permissions to return.")
     return(invisible(NULL))
-  } 
+  }
   r <- .GET(paste0(baseUrl, "/user"))
   
-  dplyr::tibble(user = httr::content(r)) %>% 
+  userInformation <- dplyr::tibble(user = httr::content(r)) %>%
     tidyr::unnest_wider(col = "user")
+  
+  return(userInformation)
 }
 
+
+#' Get my permissions
+#' 
+#' This function returns the authorized users permission for
+#' definitions like cohort, incidence rate, characterization..
+#'
+#' @template baseUrl 
+#'
+#' @return A tibble with user ID, user name, and permission information when available.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' getUserInformations(baseUrl = "http://server.org:80/WebAPI")
+#' }
+getMyPermissions <- function(baseUrl) {
+  .checkBaseUrl(baseUrl)
+  if (!securityEnabled(baseUrl)) {
+    message("Security is not enabled so there are no permissions to return.")
+    return(invisible(NULL))
+  }
+  myPermissions <- .GET(paste0(baseUrl, "/user/me")) %>%
+    httr::content()
+  
+  myPermissions <- myPermissions$permissions
+  
+  permissions <- c()
+  for (i in (1:length(myPermissions))) {
+    data <- myPermissions[[i]] %>% purrr::flatten() %>% as.data.frame() %>% dplyr::tibble()
+    permissions <- dplyr::bind_rows(permissions,
+                                    data)
+  }
+  
+  return(permissions %>% 
+           dplyr::arrange(.data$id,
+                          .data$permission))
+}
 
 #' Is WebAPI using authentication/authorization
 #'
